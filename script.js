3d44aed7190221e8f4855f27b8cd08fec9af0327
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-29 19:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const bands = [
-  "The Plot in You",
-  "The Devil Wears Prada",
-  "Pierce the Veil",
-  "Norma Jean",
-  "The Bled",
-  "Say Anything",
-  "The Midway State",
-  "We Came as Romans",
-  "Counterparts",
-  "Oh, Sleeper",
-  "A Skylit Drive",
-  "Anywhere But Here",
-  "An Old Dog",
-];
+const listContainer = document.getElementById("bands");
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']; 
+bands.sort((a, b) => stripBand(a).localeCompare(stripBand(b)));
+
+bands.forEach((band) => {
+  const li = document.createElement("li");
+  li.innerText = band;
+  listContainer.append(li);
+});
 
 function stripBand(str) {
   return str.replace(/(a|an|the)\s/i, "").trim();
 }
-const sortedBands = bands.sort((a, b) => stripBand(a).localeCompare(stripBand(b)));
-const bandsElem = document.querySelector("#bands");
-bandsElem.innerHTML = sortedBands.map((val) => `<li>${val}</li>`).join("\n");
